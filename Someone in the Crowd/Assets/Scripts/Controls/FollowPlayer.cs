8c37e6299a0508c7d5432d47bd5b20f393436a5d
Loïc Defrance <loic.defrance@gmail.com>
2017-09-30 12:44:10 +0200

    Camera constraints

--- Someone in the Crowd/Assets/Scripts/Controls/FollowPlayer.cs
+++ Someone in the Crowd/Assets/Scripts/Controls/FollowPlayer.cs
@@ -9,6 +9,8 @@ namespace SITC.Controls
         #region Members
         [SerializeField]
         private float _damping = 0f;
+        [SerializeField]
+        private Vector2 _confinementZone = new Vector2(160f, 90f);
         #endregion Members
 
         #region Private members
@@ -26,12 +28,26 @@ namespace SITC.Controls
             base.DoUpdate();
             FollowTarget();            
         }
+
+        private void OnDrawGizmos()
+        {
+            Vector3 topLeft= new Vector3(- _confinementZone.x / 2, _confinementZone.y / 2);
+            Vector3 bottomLeft = new Vector3(- _confinementZone.x / 2, - _confinementZone.y / 2);
+            Vector3 topRight = new Vector3(_confinementZone.x / 2, _confinementZone.y / 2);
+            Vector3 bottomRight = new Vector3(_confinementZone.x / 2, - _confinementZone.y / 2);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(topLeft, topRight);
+            Gizmos.DrawLine(topRight, bottomRight);
+            Gizmos.DrawLine(bottomRight, bottomLeft);
+            Gizmos.DrawLine(bottomLeft, topLeft);
+        }
         #endregion Lifecycle
 
         #region Follow player
         private void FollowTarget()
         {
-            if (! MustMove())
+            if (! CanMove())
             {
                 return;
             }
@@ -39,20 +55,44 @@ namespace SITC.Controls
             LerpPosition();
         }
 
-        private bool MustMove()
+        private bool CanMove()
         {
             if (Player == null)
             {
                 return false;
             }
-
+            
             return true;
         }
 
         private void LerpPosition()
         {
             Vector3 damped = Vector3.SmoothDamp(transform.position.SetZ(0f), Player.transform.position.SetZ(0f), ref _velocity, _damping);
+            damped = Confine(damped);
             transform.position = damped.SetZ(transform.position.z);
+        }
+
+        private Vector3 Confine(Vector3 damped)
+        {
+            if (damped.x > _confinementZone.x / 2)
+            {
+                damped.x = _confinementZone.x / 2;
+            }
+            else if (damped.x < - _confinementZone.x / 2)
+            {
+                damped.x = - _confinementZone.x / 2;
+            }
+
+            if (damped.y > _confinementZone.y / 2)
+            {
+                damped.y = _confinementZone.y / 2;
+            }
+            else if (damped.y < - _confinementZone.y / 2)
+            {
+                damped.y = - _confinementZone.y / 2;
+            }
+
+            return damped;
         }
         #endregion Follow player
     }
