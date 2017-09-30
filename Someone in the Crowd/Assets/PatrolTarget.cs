2c7cfa86686b9dce3f089f7082445cc671c2b91e
Luc Le Dauphin <ledauphin.luc@gmail.com>
2017-09-30 17:23:44 +0200

    added "LD" map & prefabs

---
+++ Someone in the Crowd/Assets/PatrolTarget.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolTarget : MonoBehaviour {
+
+    public Color color;
+
+    public bool alwaysShowGizmo;
+
+
+    private void OnDrawGizmos()
+    {
+        if (alwaysShowGizmo)
+        {
+            Gizmos.color = color;
+            Gizmos.DrawSphere(transform.position, 0.1f);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (alwaysShowGizmo == false)
+        {
+            foreach (Transform child in transform.parent)
+            {
+                Gizmos.color = color;
+                Gizmos.DrawSphere(child.position, 0.3f);
+            }
+        }
+        //_positions.ForEach(DrawTarget);
+        //Gizmos.color = AiConfiguration.AiTargetLinkGizmoColor;
+        //DrawLinks();
+    }
+}
