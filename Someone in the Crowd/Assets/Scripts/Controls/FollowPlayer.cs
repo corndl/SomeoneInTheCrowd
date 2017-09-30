59aae23a1c8395d6e2a367e524b9dc7daa711a31
Loïc Defrance <loic.defrance@gmail.com>
2017-09-30 12:26:24 +0200

    Camera Follow player with damping

---
+++ Someone in the Crowd/Assets/Scripts/Controls/FollowPlayer.cs
@@ -0,0 +1,59 @@
+﻿using SITC.Tools;
+using UnityEngine;
+
+namespace SITC.Controls
+{
+    [RequireComponent(typeof(Camera))]
+    public class FollowPlayer : SitcBehaviour
+    {
+        #region Members
+        [SerializeField]
+        private float _damping = 0f;
+        #endregion Members
+
+        #region Private members
+        private InputHandler _player = null;
+        private Vector3 _velocity = Vector3.zero;
+        #endregion Private members
+
+        #region Getters
+        private InputHandler Player { get { _player = _player ?? FindObjectOfType<InputHandler>(); return _player; } }
+        #endregion Getters
+
+        #region Lifecycle
+        protected override void DoUpdate()
+        {
+            base.DoUpdate();
+            FollowTarget();            
+        }
+        #endregion Lifecycle
+
+        #region Follow player
+        private void FollowTarget()
+        {
+            if (! MustMove())
+            {
+                return;
+            }
+
+            LerpPosition();
+        }
+
+        private bool MustMove()
+        {
+            if (Player == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void LerpPosition()
+        {
+            Vector3 damped = Vector3.SmoothDamp(transform.position.SetZ(0f), Player.transform.position.SetZ(0f), ref _velocity, _damping);
+            transform.position = damped.SetZ(transform.position.z);
+        }
+        #endregion Follow player
+    }
+}
---
+++ Someone in the Crowd/Assets/Scripts/Tools/UnityHelper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SITC.Tools
+{
+    public static class UnityHelper
+    {
+        public static Vector3 SetZ(this Vector3 vector, float z)
+        {
+            vector.z = z;
+            return vector;
+        }
+    }
+}
