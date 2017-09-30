2cf6594e459188861d5d83fc96144d31356800e6
Loïc Defrance <loic.defrance@gmail.com>
2017-09-30 10:56:07 +0200

    Can move in all directions

--- Someone in the Crowd/Assets/Scripts/Controls/InputHandler.cs
+++ Someone in the Crowd/Assets/Scripts/Controls/InputHandler.cs
@@ -19,12 +19,7 @@ namespace SITC.Controls
         {
             base.DoUpdate();
 
-            Vector3 translation = Vector3.zero;
-
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                translation = Vector3.right;
-            }
+            Vector3 translation = GetDirection();
 
             if (translation != Vector3.zero)
             {
@@ -32,5 +27,31 @@ namespace SITC.Controls
             }
         }
         #endregion Lifecycle
+
+        #region Movement
+        private Vector3 GetDirection()
+        {
+            Vector3 translation = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                translation += Vector3.right;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                translation += Vector3.left;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                translation += Vector3.down;
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                translation += Vector3.up;
+            }
+
+            return translation;
+        }
+        #endregion Movement
     }
 }
