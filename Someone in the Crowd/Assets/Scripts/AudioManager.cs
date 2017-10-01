73df729a879ab8c559860e084257cfdd2809e667
Loïc Defrance <loic.defrance@gmail.com>
2017-10-01 12:47:42 +0200

    Sounds, play music

---
+++ Someone in the Crowd/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using SITC.Tools;
+using UnityEngine;
+
+namespace SITC.Audio
+{
+    public class AudioManager : Singleton<AudioManager>
+    {
+        #region Members
+        [SerializeField]
+        private AudioSource _music = null;
+        #endregion Members
+
+        #region Lifecycle
+        protected override void Init()
+        {
+            base.Init();
+            DontDestroyOnLoad(gameObject);
+            
+        }
+        #endregion Lifecycle
+    }
+}
